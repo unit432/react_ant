19435e78a2bec445e93ec9d6126598b020d822fa
chen <cdong1@ualberta.ca>
2018-05-21 23:55:03 -0600

    Remove unnecessary div tag from SideMenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -8,21 +8,19 @@ const { Sider } = Layout
 class SideMenu extends React.Component {
   render () {
     return (
-      <div>
-        <Sider trigger={null} collapsible collapsed={this.props.sideMenuState}>
-          <div className='logo' />
-          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-            <Menu.Item key='1'>
-              <Icon type='sync' />
-              <span><Link to='/'>Downloads</Link></span>
-            </Menu.Item>
-            <Menu.Item key='2'>
-              <Icon type='setting' />
-              <span><Link to='/settings'>Settings</Link></span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-      </div>
+      <Sider trigger={null} collapsible collapsed={this.props.sideMenuState}>
+        <div className='logo' />
+        <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+          <Menu.Item key='1'>
+            <Icon type='sync' />
+            <span><Link to='/'>Downloads</Link></span>
+          </Menu.Item>
+          <Menu.Item key='2'>
+            <Icon type='setting' />
+            <span><Link to='/settings'>Settings</Link></span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
     )
   }
 }
