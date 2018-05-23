39035551408eaa020c5d696cce6d493cb22721f3
chen <cdong1@ualberta.ca>
2018-05-22 20:34:11 -0600

    Add a test for SideMenu
    Add a shallow test for SideMenu using Enzyme

--- src/components/SideMenu.test.js
+++ src/components/SideMenu.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import SideMenu from './SideMenu'
 import renderer from 'react-test-renderer'
 import { MemoryRouter } from 'react-router-dom'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
 
 describe("SideMenu", () => {
   it('SideMenu renders a Downloads and a Settings menu item', () => {
@@ -9,5 +12,10 @@ describe("SideMenu", () => {
       .create(<MemoryRouter><SideMenu /></MemoryRouter>)
       .toJSON();
     expect(rendered_menu).toMatchSnapshot()
+  })
+  
+  it('SideMenu renders a Download Button', () => {
+    const side_menu = shallow(<SideMenu />)
+    expect(side_menu.find('div').text()).toEqual('')
   })
 })
