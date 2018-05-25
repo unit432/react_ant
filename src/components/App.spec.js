58757612ced17620b9e5fde663d36dfaf8c93005
chen <cdong1@ualberta.ca>
2018-05-24 23:25:37 -0600

    Use shallow instead of mount

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -4,14 +4,14 @@ import App from './App';
 import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
-import { configure, mount } from 'enzyme'
+import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 it('render without crashing', () => {
 
   const mockStore = configureStore([]);
-  const wrapper = mount(
+  const wrapper = shallow(
     <Provider store={mockStore({})}>
       <MemoryRouter><App /></MemoryRouter>
     </Provider>
