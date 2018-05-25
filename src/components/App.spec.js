1f1e1bcf7ad4aae7327bbb2f3c663282045b1ffe
chen <cdong1@ualberta.ca>
2018-05-24 23:16:18 -0600

    Update test for App.js

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
 
-const mockStore = configureStore([]);
+it('render without crashing', () => {
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
+  const mockStore = configureStore([]);
+  const wrapper = mount(
     <Provider store={mockStore({})}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+      <MemoryRouter><App /></MemoryRouter>
+    </Provider>
+  )
+
+  expect(
+    wrapper.find('App').length
+  ).toEqual(1)
+})
