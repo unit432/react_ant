0efd1cfc84d0374ee4bdf19b6cef453f5fd225bf
chen <cdong1@ualberta.ca>
2018-05-24 23:05:09 -0600

    Add more test into SideMenu.spec.js

--- src/components/SideMenu.spec.js
+++ src/components/SideMenu.spec.js
@@ -6,8 +6,30 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 describe("SideMenu", () => {
+
+  const wrapper = mount(<MemoryRouter><SideMenu /></MemoryRouter>)
+
   it('renders a Download Sider', () => {
-    const side_menu = mount(<MemoryRouter><SideMenu /></MemoryRouter>)
-    expect(side_menu.find('Sider').length).toEqual(1)
+    expect(
+      wrapper.find('Sider').length
+    ).toEqual(1)
+  })
+
+  it('render without crashing', () => {
+    expect(
+      wrapper.length
+    ).toEqual(1)
+  })
+
+  it('render a link to settings', () => {
+    expect(
+      wrapper.find('Link').at(1).prop('to')
+    ).toEqual('/settings')
+  })
+
+  it('render a link to settings', () => {
+    expect(
+      wrapper.find('Link').at(0).prop('to')
+    ).toEqual('/')
   })
 })
