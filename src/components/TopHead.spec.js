df9c2afa021b637f84472e97107cad8e6c39e6b2
chen <cdong1@ualberta.ca>
2018-05-24 23:44:33 -0600

    Update test in TopHead.spec.js

--- src/components/TopHead.spec.js
+++ src/components/TopHead.spec.js
@@ -5,9 +5,18 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 describe("THead", () => {
+  const toggleSideMenu = jest.fn()
+  const wrapper = mount(<TopHead show_side_menu={true} toggleSideMenu={toggleSideMenu}/>)
+
   it('Renders an icon', () => {
-    const toggleSideMenu = jest.fn()
-    const head_menu = mount(<TopHead show_side_menu={true} toggleSideMenu={toggleSideMenu}/>)
-    expect(head_menu.find('Icon').length).toEqual(1)
+    expect(
+      wrapper.find('Icon').length
+    ).toEqual(1)
+  })
+
+  it('render without crashing', () => {
+    expect(
+      wrapper.find('TopHead').length
+    ).toEqual(1)
   })
 })
