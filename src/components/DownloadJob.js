9e486892ab9043fb3c6d1ebb44c0f9c9458cb839
chen <cdong1@ualberta.ca>
2018-05-28 22:31:37 -0600

    Update DownloadJob Component

--- src/components/DownloadJob.js
+++ src/components/DownloadJob.js
@@ -9,6 +9,17 @@ class DownloadJob extends React.Component {
           <Col>FileName</Col>
           <Col>ControlMenu</Col>
         </Row>
+        <Row>
+          <Col>Status</Col>
+          <Col>Download Speed</Col>
+          <Col>Upload Speed</Col>
+          <Col>Time Estimate</Col>
+          <Col>File Size</Col>
+          <Col>Downloaded Size</Col>
+          <Col>Uploaded Size</Col>
+          <Col>D/U Ratio</Col>
+          <Col>Percent</Col>
+        </Row>
         <Row><Progress precent={30} /></Row>
       </div>
     )
--- src/components/DownloadJob.spec.js
+++ src/components/DownloadJob.spec.js
@@ -4,18 +4,25 @@ import { shallow, configure, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
-describe('Download', () =>{
+describe('Download', () => {
   const job = mount(<DownloadJob />)
 
   it('renders two Rows', () => {
-    expect(job.find('Row').length).toEqual(2)
+    expect(job.find('Row').length).toEqual(3)
   })
 
   it('renders a row has columns', () => {
-    expect(job.find('Row').at(0).find('Col').length).toEqual(2)
+    const first_row = job.find('Row').at(0)
+    expect(first_row.find('Col').length).toEqual(2)
+  })
+
+  it('renders a row has download status', () => {
+    const second_row = job.find('Row').at(1)
+    expect(second_row.find('Col').length).toEqual(9)
   })
 
   it('renders a row has a progress bar', () => {
-    expect(job.find('Row').at(1).find('Progress').length).toEqual(1)
+    const third_row = job.find('Row').at(2)
+    expect(third_row.find('Progress').length).toEqual(1)
   })
 })
