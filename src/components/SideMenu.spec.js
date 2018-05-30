d9ed891cb84687606c72963fe1df5df1b9284c70
chen <cdong1@ualberta.ca>
2018-05-29 21:24:09 -0600

    Update test for SideMenu

--- src/components/SideMenu.spec.js
+++ src/components/SideMenu.spec.js
@@ -5,31 +5,56 @@ import { configure, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
+function setup() {
+  const props = {
+    sideMenuState: false
+  }
+
+  const enzymeWrapper = mount(<MemoryRouter><SideMenu {...props} /></MemoryRouter>)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
 describe("SideMenu", () => {
 
-  const wrapper = mount(<MemoryRouter><SideMenu /></MemoryRouter>)
+  const { enzymeWrapper, props } = setup()
 
   it('renders a Download Sider', () => {
     expect(
-      wrapper.find('Sider').length
+      enzymeWrapper.find('Sider').length
     ).toEqual(1)
   })
 
   it('render without crashing', () => {
     expect(
-      wrapper.length
+      enzymeWrapper.length
     ).toEqual(1)
   })
 
   it('render a link to settings', () => {
     expect(
-      wrapper.find('Link').at(1).prop('to')
+      enzymeWrapper.find('Link').at(1).prop('to')
     ).toEqual('/settings')
   })
 
   it('render a link to settings', () => {
     expect(
-      wrapper.find('Link').at(0).prop('to')
+      enzymeWrapper.find('Link').at(0).prop('to')
     ).toEqual('/')
+  })
+
+  it('fold/unfold Sider according to sideMenuState', () => {
+    const sider = enzymeWrapper.find('Sider').at(0)
+    expect(
+      sider.prop('collapsed')
+    ).toEqual(false)
+
+    const wrapper = mount(<MemoryRouter><SideMenu sideMenuState={true} /></MemoryRouter>)
+    expect(
+      wrapper.find('Sider').at(0).prop('collapsed')
+    ).toEqual(true)
   })
 })
