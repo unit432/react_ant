070f8ffb1c58f56e9da031553a6964216cd4bf2f
chen <cdong1@ualberta.ca>
2018-05-30 23:18:10 -0600

    Update Download component

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Row } from 'antd'
+import DownloadJob from './DownloadJob'
 
 class Downloads extends React.Component {
   render () {
+    const jobList = this.props.jobs
+    let downloadJobs = ''
+
+    if (jobList && jobList.length > 0)
+    {
+      downloadJobs = this.props.jobs.map(
+        (job) => <Row key={job.gid}><DownloadJob {...job}/></Row>
+      )
+    }
+    else
+    {
+      downloadJobs = <span>No Downloads</span>
+    }
+
     return (
-      <h1>Downloads</h1>
+      <div>
+        {downloadJobs}
+      </div>
     )
   }
 }
---
+++ src/components/Downloads.spec.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import Downloads from './Downloads'
+import { mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
+
+const jobs =
+  [
+    {
+      gid: '2089b05ecca3d829',
+      fileName: 'debian.iso',
+      status: 'active',
+      downloadSpeed: '120 KB/s',
+      uploadSpeed: '12 KB/s',
+      totalLength: '1G',
+      completedLength: '50M',
+      uploadLength: '12M'
+    },
+    {
+      gid: '3089b05ecca3d829',
+      fileName: 'ubuntu.iso',
+      status: 'paused',
+      downloadSpeed: '120 KB/s',
+      uploadSpeed: '12 KB/s',
+      totalLength: '1G',
+      completedLength: '500M',
+      uploadLength: '120M'
+    }
+  ]
+
+function setup() {
+  const props = {
+    jobs: jobs
+  }
+
+  const enzymeWrapper = mount(<Downloads {...props} />)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('Downloads', () => {
+  const { enzymeWrapper } = setup()
+
+  it('render without crashing', () => {
+    expect(
+      enzymeWrapper.find('Downloads').length
+    ).toEqual(1)
+  })
+
+  it('render two DownloadJobs', () => {
+    expect(
+      enzymeWrapper.find('DownloadJob').length
+    ).toEqual(2)
+  })
+
+  it('render a notice when there no jobs', () => {
+    const wrapper = mount(<Downloads jobs={ [] } />)
+
+    expect(
+      wrapper.find('Downloads').find('span').at(0).text()
+    ).toEqual('No Downloads')
+  })
+})
