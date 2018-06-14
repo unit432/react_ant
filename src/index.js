3c65079be589300fc677412432ce5a48233c9bdc
chen <cdong1@ualberta.ca>
2018-06-13 20:53:12 -0600

    Setup Redux DevTools

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers'
 import { BrowserRouter as Router } from 'react-router-dom'
 import rootSaga  from './sagas'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 )
 
 sagaMiddleware.run(rootSaga)
