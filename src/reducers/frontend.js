1fb9c5f36b9213f27c8c350bb7b22300b2d7b01e
chen <cdong1@ualberta.ca>
2018-06-14 23:19:14 -0600

    Update frontend reducer

--- src/reducers/frontend.js
+++ src/reducers/frontend.js
@@ -1,7 +1,16 @@
-const frontend = (state = false, action) => {
+const frontend = (state =
+  {
+    showSideMenu: true,
+    isFetching: false,
+    fetchFailed: false
+  }, action) => {
   switch (action.type) {
     case 'TOGGLE_SIDE_MENU':
-      return !state
+      return Object.assign({}, state, { showSideMenu: !state.showSideMenu })
+    case 'FETCH_JOBS_REQUEST':
+      return Object.assign({}, state, { isFetching: !state.isFetching })
+    case 'FETCH_JOBS_FAILURE':
+      return Object.assign({}, state, { fetchFailed: !state.fetchFailed })
     default:
       return state
   }
--- src/reducers/frontend.spec.js
+++ src/reducers/frontend.spec.js
@@ -1,15 +1,43 @@
 import frontend from './frontend'
+import {
+  TOGGLE_SIDE_MENU,
+  FETCH_JOBS_REQUEST,
+  FETCH_JOBS_FAILURE,
+  FETCH_JOBS_SUCCESS
+} from '../actions'
 
 describe('frontend reducers', () => {
+  let initialState
+
+  beforeEach (() => {
+    initialState = {
+      showSideMenu: true,
+      isFetching: false,
+      fetchFailed: false
+    }
+  })
+
   it('should handle initial state', () => {
     expect(
-      frontend(true, {})
-    ).toEqual(true)
+      frontend(initialState, {})
+    ).toEqual(initialState)
   })
 
   it('should toggle hide_side_menu state', () => {
     expect(
-      frontend(false, { type: 'TOGGLE_SIDE_MENU' })
+      frontend(initialState, { type: TOGGLE_SIDE_MENU }).showSideMenu
+    ).toEqual(false)
+  })
+
+  it('should handle isFetching state', () => {
+    expect(
+      frontend(initialState, { type: FETCH_JOBS_REQUEST }).isFetching
+    ).toEqual(true)
+  })
+
+  it('should handle fetchFailed state', () => {
+    expect(
+      frontend(initialState, { type: FETCH_JOBS_FAILURE }).fetchFailed
     ).toEqual(true)
   })
 })
