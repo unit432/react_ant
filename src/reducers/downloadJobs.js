08e734767d29c3b0ac51dd49997dccdee0491b82
chen <cdong1@ualberta.ca>
2018-06-19 20:59:47 -0600

    Update reducer to handle merging arrays

--- src/reducers/downloadJobs.js
+++ src/reducers/downloadJobs.js
@@ -3,10 +3,12 @@ import { LOAD_JOBS } from '../actions/actionTypes'
 const downloadJobs = (state = [], action) => {
   switch (action.type) {
     case LOAD_JOBS:
-      return [
-        ...state,
-        ...action.array
-      ]
+      let merged_arr = []
+      for (let element of state) {
+        if (!action.array.map(x => x.uid).includes(element.uid))
+          merged_arr.push(element)
+      }
+      return merged_arr.concat(action.array)
     default:
       return state
   }
--- src/reducers/downloadJobs.spec.js
+++ src/reducers/downloadJobs.spec.js
@@ -5,7 +5,7 @@ describe('downloads reducers', () => {
 
   const fetchedJobs = {
     type: LOAD_JOBS,
-    array: [{ uid: 'b' }, { uid: 'a' }]
+    array: [{ uid: 'b' }, { uid: 'a', downloadSpeed: "1152" }]
   }
 
   it('should handle empty array', () => {
@@ -13,7 +13,7 @@ describe('downloads reducers', () => {
       downloadJobs([], fetchedJobs)
     ).toEqual([
       { uid: 'b' },
-      { uid: 'a' }
+      { uid: 'a', downloadSpeed: "1152" }
     ])
   })
 
@@ -23,7 +23,17 @@ describe('downloads reducers', () => {
       { uid: 'u3' },
       { uid: 'u4' },
       { uid: 'b' },
-      { uid: 'a' }
+      { uid: 'a', downloadSpeed: "1152" }
+    ])
+  })
+
+  it('should update existing array with content from new array', () => {
+    const currentJobs = [ { uid: 'u3' }, { uid: 'u4' }, { uid: 'a', downloadSpeed: "10" } ]
+    expect(downloadJobs(currentJobs, fetchedJobs)).toEqual([
+      { uid: 'u3' },
+      { uid: 'u4' },
+      { uid: 'b' },
+      { uid: 'a', downloadSpeed: "1152" }
     ])
   })
 })
