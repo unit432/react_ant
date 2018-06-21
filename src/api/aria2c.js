bafd1b9cb56043c22bc64778d50c461a4d1056ee
chen <cdong1@ualberta.ca>
2018-06-20 21:10:28 -0600

    Add requestFactory

--- src/api/aria2c.js
+++ src/api/aria2c.js
@@ -5,4 +5,15 @@ const fetchData = () => {
   return axios.get(baseURL, { params: { 'jsonrpc': '2.0', 'method':'aria2.tellActive', 'id':'netant' } })
 }
 
+export const requestFactory = (name, params) => {
+  var jsonreq = {
+    jsonrpc: '2.0',
+    id: 'netant',
+    method: 'aria2.' + name,
+    params: params
+  }
+
+  return jsonreq
+}
+
 export default fetchData
--- src/api/aria2c.spec.js
+++ src/api/aria2c.spec.js
@@ -1,6 +1,23 @@
-import fetchJobs from './aria2c'
+import { fetchJobs, requestFactory } from './aria2c'
 
 describe('fetchJob', () => {
-  it('fetches data from aria2c', () => {
+  describe('requestFactory', () => {
+    let jsonreq
+
+    beforeEach(() => {
+      jsonreq = { jsonrpc: '2.0', id: 'netant' }
+    })
+
+    it('builds tellActive', () => {
+      jsonreq.method = 'aria2.tellActive'
+      expect(requestFactory('tellActive')).toEqual(jsonreq)
+    })
+
+    it('builds addUri', () => {
+      jsonreq.method = 'aria2.addUri'
+      const params = [['http://example.org/file']]
+      jsonreq.params = params
+      expect(requestFactory('addUri', params)).toEqual(jsonreq)
+    })
   })
 })
