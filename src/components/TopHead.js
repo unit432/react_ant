233892753546b3927ba91fede79151ad509a55c3
chen <cdong1@ualberta.ca>
2018-08-27 19:41:32 -0600

    Update icon style on top Header.
    Redirect to job progress panel after job is added.

--- src/components/TopHead.js
+++ src/components/TopHead.js
@@ -1,33 +1,37 @@
 import React from "react";
-import { Layout, Icon, Menu } from "antd";
+import { Layout, Icon, Tooltip } from "antd";
 import { Link } from "react-router-dom";
 
 const { Header } = Layout;
-const MenuItem = Menu.Item;
 
 class TopHead extends React.Component {
   render() {
     return (
       <Header style={{ background: "#fff", padding: 0 }}>
-        <Menu mode="horizontal">
-          <MenuItem>
-            <Icon
-              type={this.props.showSideMenu ? "menu-unfold" : "menu-fold"}
-              onClick={this.props.toggleSideMenu}
-            />
-          </MenuItem>
-          <MenuItem>
+        <span className="topIconSpan">
+          <Icon
+            className="topIcon"
+            type={this.props.showSideMenu ? "menu-unfold" : "menu-fold"}
+            onClick={this.props.toggleSideMenu}
+          />
+        </span>
+        <span className="topIconSpan">
+          <Tooltip placement="top" title="Start">
             <Link to="/job-form">
-              <Icon type="file-add" />
+              <Icon className="topIcon" type="file-add" />
             </Link>
-          </MenuItem>
-          <MenuItem>
-            <Icon type="pause-circle-o" />
-          </MenuItem>
-          <MenuItem>
-            <Icon type="delete" />
-          </MenuItem>
-        </Menu>
+          </Tooltip>
+        </span>
+        <span className="topIconSpan">
+          <Tooltip placement="top" title="Pause">
+            <Icon className="topIcon" type="pause-circle-o" />
+          </Tooltip>
+        </span>
+        <span className="topIconSpan">
+          <Tooltip placement="top" title="Delete">
+            <Icon className="topIcon" type="delete" />
+          </Tooltip>
+        </span>
       </Header>
     );
   }
--- src/components/UriForm.js
+++ src/components/UriForm.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
+import { Redirect } from "react-router";
 
 const { TextArea } = Input;
 const FormItem = Form.Item;
 
 class UriJobForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobSubmitted: false
+    };
+  }
+
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -13,12 +21,17 @@ class UriJobForm extends React.Component {
         for (var element of uris) {
           this.props.addUris([element]);
         }
+        this.setState({ jobSubmitted: true });
       }
     });
   };
 
   render() {
     const { getFieldDecorator } = this.props.form;
+
+    if (this.state.jobSubmitted) {
+      return <Redirect to="/" />;
+    }
 
     return (
       <Form onSubmit={this.handleSubmit}>
